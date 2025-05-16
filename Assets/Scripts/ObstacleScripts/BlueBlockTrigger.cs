f2d17ea8ad756de2b17e41113043e44dc8c8c134
YunusErdemGokdag <120821444+panheadd@users.noreply.github.com>
2025-05-16 18:50:58 +0300

    camera shake

--- Assets/Scripts/ObstacleScripts/BlueBlockTrigger.cs
+++ Assets/Scripts/ObstacleScripts/BlueBlockTrigger.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class BlueBlockTrigger : MonoBehaviour
 {
+    public GameObject camera ;
     public BlueBlock blueBlock;
     // Start is called before the first frame update
     void Start()
@@ -21,7 +22,27 @@ public class BlueBlockTrigger : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
+            StartCoroutine(Shake(2f, 0.1f));
             blueBlock.playerOnBlock = true;
         }
+    }
+
+    public IEnumerator Shake(float duration, float magnitude)
+    {
+        Vector3 originalPos = camera.transform.localPosition;
+        float elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            float offsetX = Random.Range(-1f, 1f) * magnitude;
+            float offsetY = Random.Range(-1f, 1f) * magnitude;
+
+            camera.transform.localPosition = originalPos + new Vector3(offsetX, offsetY, 0f);
+
+            elapsed = elapsed+ Time.deltaTime;
+            yield return null;
+        }
+
+        camera.transform.localPosition = originalPos;
     }
 }
